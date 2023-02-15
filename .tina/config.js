1d09665e4ee13eff9817dae014728bd429112ca6
Agnes Cameron <agnesfcameron@gmail.com>
2023-02-15 11:57:06 +0000

    forestry to tina

---
+++ .tina/config.js
@@ -0,0 +1,108 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "7d7b633d-cc5b-4d55-89f9-6acba1bcae9c", // Get this from tina.io
+  token: "f3da1744501c7a3ba478881986397c7f5e465f80", // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "./",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "./",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        label: "Pages-en",
+        name: "pages_en",
+        path: "_pages/en",
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        label: "Campaigns-en",
+        name: "campaigns_en",
+        path: "_campaigns/en",
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        label: "News-en",
+        name: "news_en",
+        path: "_news/en",
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        label: "Pages-es",
+        name: "pages_es",
+        path: "_pages/es",
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        label: "Campaigns-es",
+        name: "campaigns_es",
+        path: "_campaigns/es",
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        label: "News-es",
+        name: "news_es",
+        path: "_news/es",
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
